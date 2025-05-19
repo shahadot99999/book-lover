3c7c893b07691bc571e771b479e7159d02306f54
shahadot99999 <shahadot99999@gmail.com>
2025-05-19 20:11:03 +0600

    load fake data to display books

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+import Books from "../Books/Books";
 
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home page.</h2>
+           <Banner></Banner>
+           <Books></Books>
         </div>
     );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
+import { NavLink } from "react-router-dom";
 
 
 
 const NavBar = () => {
 
     const links = <>
-        <li><a>Home</a></li>
+        <li><NavLink to="/">Home</NavLink></li>
+        
         <li><a>About</a></li>
     </>
     return (
