4430d746eb574f975f031035991235cf17687b4e
shahadot99999 <shahadot99999@gmail.com>
2025-05-19 08:08:12 +0600

    React Router Tailwind Daisy Ui Setup For Boi Poka

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+
+
+const Root = () => {
+    return (
+        <div>
+            <h3>Hello from Root.</h3>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+//import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
